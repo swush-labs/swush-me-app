f003edeca6660fffbbebfae6a1bd00e82feb443c
muddlebee <anweshknayak@gmail.com>
2024-10-22 07:29:36 +0530

    feat(gh-pages): fix and lint fix

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/swush-me-app' : '',
+}
+
+module.exports = nextConfig
